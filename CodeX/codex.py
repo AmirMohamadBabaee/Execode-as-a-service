98254e69436f90867cf000fb22aaeed9454a4994
Amir01 <iran.volleyball79@gmail.com>
2023-03-08 12:57:37 +0330

    add: add codex request function

---
+++ CodeX/codex.py
@@ -0,0 +1,36 @@
+import os
+import requests
+
+
+def codex_request(filename: str, input_str: str=''):
+    if not os.path.exists(filename):
+        print(f'{filename} does not exist')
+        return {}
+    
+    CodeX_URL     = "https://api.codex.jaagrav.in"
+    
+    _, language = os.path.splitext(filename)
+    if language:
+        language = language[1:]
+
+    source_code = None
+    with open(filename, 'r') as f:
+        source_code = f.read()
+
+    data = {
+        'code': source_code,
+        'language': language, 
+        'input': input_str
+    }
+
+    resp = requests.request(
+        method='post', 
+        url=CodeX_URL, 
+        data=data, 
+        headers={
+        'content-type': 'application/x-www-form-urlencoded'
+        }
+    )
+    return resp.text
+
+print(codex_request('test.py'))
